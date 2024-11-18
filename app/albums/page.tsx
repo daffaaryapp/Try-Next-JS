7efd49518389805eb53a7591915296d954ccbd57
daffa <daffaryasatya28@gmail.com>
2024-11-18 22:00:00 +0700

    init 19.22

---
+++ app/albums/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const albums = () => {
+  return (
+    <div>
+      Album
+    </div>
+  )
+}
+
+export default albums
---
+++ app/posts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Posts = () => {
+  return (
+    <div>
+      Postingan Pages
+    </div>
+  )
+}
+
+export default Posts
