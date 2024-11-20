f5df9b32d2e8264ba32ed0c5e2db8f07c5249c13
daffa <daffaryasatya28@gmail.com>
2024-11-20 09:25:49 +0700

    finish

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React from 'react';
+import styles from './postPage.module.css';
+import CardList from '../components/Posts/CardList';
+import ViewUserButton from '../components/Posts/ViewUserButton';
+const base_url = 'https://jsonplaceholder.typicode.com/posts'
 
-const Posts = () => {
+
+//untuk validasi
+interface Iposts {
+  userId: number,
+  id:number,
+  title: string,
+  body: string
+}
+
+const Posts = async() => {
+  
+  const response = await fetch(base_url, {
+    cache: 'no-store'
+  })
+  const posts:Iposts[] = await response.json()
+
+
   return (
-    <div>
-      Postingan Pages
-    </div>
+    <>
+      <h1 className='bg-fuchsia-300'>
+        Postingan Pages
+      </h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      {posts.map((post)=>{
+        return (
+          <CardList key={post.id}>
+        <p>{post.id}</p>
+        <p>{post.title}</p>
+        <p>{post.body}</p>
+        <ViewUserButton userId={post.userId}/>
+      </CardList>
+        )
+      })}
+      
+    </>
   )
 }
 
